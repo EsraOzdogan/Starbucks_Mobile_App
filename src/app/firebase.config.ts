f6696d4ddb9b523dbae81ce7abf31778150ab6f2
EsraOzdogan <esraozdogaan@gmail.com>
2020-10-04 20:53:07 +0300

    firebase.config.ts

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,11 @@
+//config default
+export const firebaseConfig = {
+  apiKey: "AIzaSyB5pEE68ps5yjImKR-U0IS_lRpZJ7ZBqeE",
+    authDomain: "star-44c9b.firebaseapp.com",
+    databaseURL: "https://star-44c9b.firebaseio.com",
+    projectId: "star-44c9b",
+    storageBucket: "star-44c9b.appspot.com",
+    messagingSenderId: "973470223763",
+    appId: "1:973470223763:web:efc0593fcaa1361fefec66",
+    measurementId: "G-XYHZYN5TXH"
+};
