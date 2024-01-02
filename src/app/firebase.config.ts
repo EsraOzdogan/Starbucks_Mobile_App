f35326831afdea96823797eb57d7909bd083ae30
Esra Özdoğan <50195220+EsraOzdogan@users.noreply.github.com>
2024-01-03 00:47:52 +0300

    Update firebase.config.ts

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,11 +1,11 @@
 //config default
 export const firebaseConfig = {
-  apiKey: "AIzaSyBBfGZtmXQ3zpoRgOExzf89DPsVshuFqdc",
-    authDomain: "starbucks-b3221.firebaseapp.com",
-    databaseURL: "https://starbucks-b3221.firebaseio.com",
-    projectId: "starbucks-b3221",
-    storageBucket: "starbucks-b3221.appspot.com",
-    messagingSenderId: "608337985854",
-    appId: "1:608337985854:web:a02cc1e9721e5fbc01ec6f",
-    measurementId: "G-6WH42GKKF1"
+  apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
